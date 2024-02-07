460c3ebf7f9d17e18acfdb0588c5b84f72e73be6
Mateus <mateuscavalcant7@gmail.com>
2024-02-07 14:38:21 -0300

    comments added

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
+from flask import Flask, render_template, request, json
 from server.analysis import analyze_sentiment
-from flask import Flask, request, render_template, json
+
 
 app = Flask(__name__)
 
+# Route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Route for analyzing sentiment
 @app.route('/analyze_sentiment', methods=['POST'])
 def analyze_sentiment_route():
+    # Get the content from the request
     content = request.json.get("content", "")
 
     # Analyze the sentiment of the content
     sentiment = analyze_sentiment(content)
 
-    # Send the result back to the client
+    # Send the sentiment result back to the client
     response = json.dumps({"sentiment": sentiment})
     return response
 
 
 if __name__ == "__main__":
+    # Run the Flask app
     app.run(host='0.0.0.0', port=8765, debug=True, threaded=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 function sendMessage() {
     const messageInput = document.getElementById('messageInput').value;
 
-    // Send the message input to the sentiment analysis route
     fetch('/analyze_sentiment', {
         method: 'POST',
         headers: {
@@ -9,29 +8,26 @@ function sendMessage() {
         },
         body: JSON.stringify({ "content": messageInput }),
     })
-    .then(response => response.json())
-    .then(data => {
-        const responseElement = document.getElementById('response');
-        responseElement.innerHTML = data.sentiment;
+        .then(response => response.json())
+        .then(data => {
+            const responseElement = document.getElementById('response');
+            responseElement.innerHTML = data.sentiment;
 
-        // Change the color of the response element based on sentiment
-        if (data.sentiment === "Positive") {
-            responseElement.style.color = 'green';
-        } else if (data.sentiment === "Negative"){
-            responseElement.style.color = 'red';
-        } else {
-            responseElement.style.color = 'white';
-        }
-    })
-    .catch(error => {
-        console.error('An error occurred:', error);
-    });
+            if (data.sentiment === "Positive") {
+                responseElement.style.color = 'green';
+            } else if (data.sentiment === "Negative") {
+                responseElement.style.color = 'red';
+            } else {
+                responseElement.style.color = 'white';
+            }
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
 }
 
 function autoResize() {
     const textarea = document.getElementById('messageInput');
-    textarea.style.height = 'auto'; // Reset the height to recalculate correctly
-
-    // Set the height of the textarea based on content and scroll height
+    textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
 }
